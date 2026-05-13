73cdd46630b7362d15e8876cb72ca763b381d0e0
Diogo Pereira <diogo.pereira@ezoqc.com>
2026-05-13 11:11:25 -0400

    server hotreload

--- 03 - volumes/1 - data-volumes/server.js
+++ 03 - volumes/1 - data-volumes/server.js
@@ -22,6 +22,8 @@ app.get('/exists', (req, res) => {
   res.sendFile(filePath);
 });
 
+console.log('runtime change')
+
 app.post('/create', async (req, res) => {
   const title = req.body.title;
   const content = req.body.text;
